a2ae4337daa4ad2a9ab5c473f72f8884a1e163fe
Devon Wilson <djw4s@mtmail.mtsu.edu>
2021-03-04 00:53:25 -0600

    Adds turret look at target behavior, and a line renderer that points from the barrel to the target.

---
+++ Assets/Scripts/Turret/TurretRotation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotation : MonoBehaviour
+{
+    [Header("Turret Parts")]
+    [SerializeField] private Transform _connectionPoint;
+    [SerializeField] private Transform _barrelInerior;
+
+    [Header("Targeting")]
+    [SerializeField] private Transform _trarget;
+    [SerializeField] private LineRenderer _lineRenderer;
+
+    private void Start()
+    {
+        _lineRenderer.positionCount = 2;
+    }
+
+    private void Update()
+    {
+        _connectionPoint.LookAt(_trarget, Vector3.up);
+        _lineRenderer.SetPosition(0, _barrelInerior.position);
+        _lineRenderer.SetPosition(1, _trarget.position);
+    }
+}
